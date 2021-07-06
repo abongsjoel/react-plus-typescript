295879064162f28431b8c28bc0a0c15c99cc6717
tj-ubuntu <abongsjoel@gmail.com>
2021-07-06 16:28:40 +0100

    Create App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const App = () => {
-  (window as any).hello();
-  return <div>Hello World</div>;
-};
+import App from "./components/App";
 
 ReactDom.render(<App />, document.getElementById("root"));
