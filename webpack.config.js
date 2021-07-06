4ff9d7054a884bd060858cf7c353d4ef3fe84792
tj-ubuntu <abongsjoel@gmail.com>
2021-07-06 15:06:53 +0100

    Setup Typescript using ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
