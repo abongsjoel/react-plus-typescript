af346755b940e6a72d425e7d2c7ff269bd8938fe
tj-ubuntu <abongsjoel@gmail.com>
2021-07-06 16:53:58 +0100

    Function components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import pizzas from "../data/pizzas.json";
+import Pizza from "./Pizza";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <ul>
+      {pizzas.map((pizza) => (
+        <Pizza key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
 };
 
 export default App;
