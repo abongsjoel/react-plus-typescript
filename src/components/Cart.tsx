f8fe1ef05341a6dd00cb59a9eeaa29e492e22e61
tj-ubuntu <abongsjoel@gmail.com>
2021-07-08 17:56:57 +0100

    Use React context and the useState hook

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -47,7 +47,11 @@ class Cart extends React.Component<Props, State> {
               >
                 <ul>
                   {state.cart.items.map((item) => {
-                    return <li key={item.id}>{item.name}</li>;
+                    return (
+                      <li key={item.id}>
+                        {item.name} &times; {item.quantity}
+                      </li>
+                    );
                   })}
                 </ul>
               </div>
--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -19,14 +19,27 @@ const Pizza: React.FC<Props> = ({ pizza }: Props) => {
   const setState = useSetState();
   const handleAddToCartClick = () => {
     setState((state) => {
+      const itemExists = state.cart.items.find((item) => item.id === pizza.id);
       return {
         ...state,
         cart: {
           ...state.cart,
-          items: [
-            ...state.cart.items,
-            { id: pizza.id, name: pizza.name, price: pizza.price },
-          ],
+          items: itemExists
+            ? state.cart.items.map((item) => {
+                if (item.id === pizza.id) {
+                  return { ...item, quantity: item.quantity + 1 };
+                }
+                return item;
+              })
+            : [
+                ...state.cart.items,
+                {
+                  id: pizza.id,
+                  name: pizza.name,
+                  price: pizza.price,
+                  quantity: 1,
+                },
+              ],
         },
       };
     });
