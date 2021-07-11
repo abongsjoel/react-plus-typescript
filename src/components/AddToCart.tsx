987d1215680b1f7c9ca809728b94fd323103827a
tj-ubuntu <abongsjoel@gmail.com>
2021-07-11 21:18:28 +0100

    Create a render Props component

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -29,3 +29,19 @@ export function withAddToCart<OriginalProps extends AddToCartProps>(
 
   return AddToCartHOC;
 }
+
+export const WithAddToCartProps: React.FC<{
+  children: (props: AddToCartProps) => JSX.Element;
+}> = ({ children }) => {
+  const dispatch = useStateDispatch();
+
+  const addToCart: AddToCartProps["addToCart"] = (item) => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: {
+        item,
+      },
+    });
+  };
+  return children({ addToCart });
+};
--- src/components/SpecialOffer.tsx
+++ src/components/SpecialOffer.tsx
@@ -1,31 +1,42 @@
 import React from "react";
 import { Pizza } from "../types";
-import { AddToCartProps, withAddToCart } from "./AddToCart";
+import { AddToCartProps, WithAddToCartProps } from "./AddToCart";
 
 import SpecialOfferCSS from "./SpecialOffer.module.css";
 
-interface Props extends AddToCartProps {
+interface Props {
   pizza: Pizza;
 }
 
-const SpecialOffer: React.FC<Props> = ({ pizza, addToCart }) => {
-  const handleAddToCartClick = () => {
-    // addToCart({ id: pizza.id, name: pizza.name, price: pizza.price})
-    addToCart(pizza);
-  };
-
+const SpecialOffer: React.FC<Props> = ({ pizza }) => {
   return (
     <div className={SpecialOfferCSS.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <p>{pizza.price}</p>
-        <button type="button" onClick={handleAddToCartClick}>
-          Add to Cart
-        </button>
+        <WithAddToCartProps>
+          {({ addToCart }) => {
+            return (
+              <button
+                type="button"
+                onClick={() =>
+                  // addToCart({
+                  //   id: pizza.id,
+                  //   name: pizza.name,
+                  //   price: pizza.price,
+                  // })
+                  addToCart(pizza)
+                }
+              >
+                Add to Cart
+              </button>
+            );
+          }}
+        </WithAddToCartProps>
       </div>
     </div>
   );
 };
 
-export default withAddToCart(SpecialOffer);
+export default SpecialOffer;
